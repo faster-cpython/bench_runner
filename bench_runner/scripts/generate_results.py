4956b246488857087256c904b08d1738dc368d5a
Michael Droettboom <mdboom@gmail.com>
2024-03-19 17:05:22 -0400

    lint

--- bench_runner/scripts/generate_results.py
+++ bench_runner/scripts/generate_results.py
@@ -207,7 +207,8 @@ def send_notification(people_affected):
     for actor, directories in people_affected.items():
         body += f"@{actor}: "
         for directory in directories:
-            body += f"[New results](https://github.com/{github_repo}-public/tree/main/{directory}) "
+            body += "[New results]"
+            body += f"(https://github.com/{github_repo}-public/tree/main/{directory}) "
         body += "\n"
     body += "\nNOTE: It may take up to 5 minutes before results are published."
 
