e178ec019c596a5f849c8987f3f4923049488ea8
Michael Droettboom <mdboom@gmail.com>
2024-05-01 11:20:36 -0400

    Remove print statements

--- bench_runner/scripts/should_run.py
+++ bench_runner/scripts/should_run.py
@@ -87,10 +87,6 @@ def main():
     parser.add_argument("jit")
     args = parser.parse_args()
 
-    print(repr(args.tier2))
-    print(repr(args.jit))
-    print(args.tier2 != "false", args.jit != "false")
-
     _main(
         args.force != "false",
         args.fork,
