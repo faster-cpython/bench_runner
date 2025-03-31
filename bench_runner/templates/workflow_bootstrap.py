bc6b24be89acce2d3d001e0e43f2b6e0f17944a1
Michael Droettboom <mdboom@gmail.com>
2025-03-31 14:25:45 -0400

    Simplify bootstrapping

--- bench_runner/templates/workflow_bootstrap.py
+++ bench_runner/templates/workflow_bootstrap.py
@@ -6,7 +6,6 @@
 # the error message that the version of Python is too old.
 
 
-import argparse
 from pathlib import Path
 import shutil
 import subprocess
@@ -54,26 +53,7 @@ def install_requirements(venv: Path) -> None:
     run_in_venv(venv, "pip", ["install", "-r", "requirements.txt"])
 
 
-def _main(
-    fork: str,
-    ref: str,
-    machine: str,
-    benchmarks: str,
-    flags: str,
-    force: bool,
-    pgo: bool,
-    perf: bool,
-    pystats: bool,
-    force_32bit: bool,
-    run_id: str | None = None,
-):
-    if force_32bit and sys.platform != "win32":
-        raise RuntimeError("32-bit builds are only supported on Windows")
-    if perf and not sys.platform.startswith("linux"):
-        raise RuntimeError("perf profiling is only supported on Linux")
-    if pystats and not sys.platform.startswith("linux"):
-        raise RuntimeError("Pystats is only supported on Linux")
-
+def main():
     venv = Path("venv")
     create_venv(venv)
     install_requirements(venv)
@@ -81,75 +61,13 @@ def _main(
     # Now that we've installed the full bench_runner library,
     # continue on in a new process...
 
-    args = ["workflow", fork, ref, machine, benchmarks, flags]
-    if force:
-        args.append("--force")
-    if pgo:
-        args.append("--pgo")
-    if perf:
-        args.append("--perf")
-    if pystats:
-        args.append("--pystats")
-    if force_32bit:
-        args.append("--32bit")
-    if run_id:
-        args.extend(["--run_id", run_id])
+    last_arg = sys.argv.find("workflow_bootstrap.py")
+    if last_arg == -1:
+        raise ValueError(
+            "The script should be run from the command line with the workflow_bootstrap.py argument"
+        )
 
-    run_in_venv(venv, "bench_runner", args)
-
-
-def main():
-    parser = argparse.ArgumentParser(
-        description="""
-        Run the full compile/benchmark workflow.
-        """,
-    )
-    parser.add_argument("fork", help="The fork of CPython")
-    parser.add_argument("ref", help="The git ref in the fork")
-    parser.add_argument(
-        "machine",
-        help="The machine to run the benchmarks on.",
-    )
-    parser.add_argument("benchmarks", help="The benchmarks to run")
-    parser.add_argument("flags", help="Configuration flags")
-    parser.add_argument("--force", action="store_true", help="Force a re-run")
-    parser.add_argument(
-        "--pgo",
-        action="store_true",
-        help="Build with profiling guided optimization",
-    )
-    parser.add_argument(
-        "--perf",
-        action="store_true",
-        help="Collect Linux perf profiling data (Linux only)",
-    )
-    parser.add_argument(
-        "--pystats",
-        action="store_true",
-        help="Enable Pystats (Linux only)",
-    )
-    parser.add_argument(
-        "--32bit",
-        action="store_true",
-        dest="force_32bit",
-        help="Do a 32-bit build (Windows only)",
-    )
-    parser.add_argument("--run_id", default=None, type=str, help="The github run id")
-    args = parser.parse_args()
-
-    _main(
-        args.fork,
-        args.ref,
-        args.machine,
-        args.benchmarks,
-        args.flags,
-        args.force,
-        args.pgo,
-        args.perf,
-        args.pystats,
-        args.force_32bit,
-        args.run_id,
-    )
+    run_in_venv(venv, "bench_runner", ["workflow", *sys.argv[last_arg + 1 :]])
 
 
 if __name__ == "__main__":
